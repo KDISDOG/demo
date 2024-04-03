6269d4f0a5a4662756ad7ffa345f3a1cf13fae90
KDISDOG <sam90315@gmail.com>
2024-04-03 20:33:48 +0800

    0403

---
+++ practice/0402test/src/App.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav class="bg-blue-800">
+      <ul class="flex flex-row text-white gap-5">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <div class="flex md:flex-row flex-col top-0 left-0 w-full">
+      <div class="flex flex-1 bg-blue-400 items-center justify-center">
+        <img src="../public/26_101119598844.jpg" alt="" class="w-[300px]" />
+      </div>
+      <div class="flex flex-1 bg-blue-400 items-center justify-center">
+        <p class="text-3xl">
+          六福村真好玩六福村真好玩六福村真好玩六福村真好玩六福村真好玩
+          六福村真好玩 六福村真好玩 六福村真好玩 六福村真好玩 六福村真好玩
+        </p>
+      </div>
+    </div>
+    <div class="flex md:flex-row l flex-wrap top-0 left-0 w-full">
+      <div class="flex flex-1 bg-blue-400 items-center justify-center">
+        <p class="text-3xl">
+          六福村真好玩六福村真好玩六福村真好玩六福村真好玩六福村真好玩
+          六福村真好玩 六福村真好玩 六福村真好玩 六福村真好玩 六福村真好玩
+        </p>
+      </div>
+      <div class="flex flex-1 bg-blue-400 items-center justify-center">
+        <img src="../public/26_101119598844.jpg" alt="" class="w-[300px]" />
+      </div>
+      <div class="flex flex-1 bg-blue-400 items-center justify-center">
+        <img src="../public/26_101119598844.jpg" alt="" class="w-[300px]" />
+      </div>
+    </div>
+  </main>
+</template>
+<style></style>
