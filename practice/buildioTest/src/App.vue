ad6bffe13b8e02ad43b13896e646494b4ce566ce
KDISDOG <sam90315@gmail.com>
2024-04-16 17:34:58 +0800

    0416

---
+++ practice/buildioTest/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex flex-col pb-20 bg-white">
+    <div
+      class="flex z-10 flex-col items-end px-16 pt-11 pb-3 w-full text-xl text-center bg-fuchsia-700 text-white text-opacity-70 max-md:px-5 max-md:max-w-full"
+    >
+      <div class="flex flex-col mr-40 max-w-full w-[566px] max-md:mr-2.5">
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/71374ec425371e094a899cd257be15a2540429b5593ea00977a6fdab26e5f8b1?apiKey=1ae664018bfa43d7a4184d6c2cb38528&"
+          class="self-end aspect-square fill-white w-[97px]"
+        />
+        <div class="mt-1 max-md:max-w-full">click the cards!!</div>
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-start px-16 pt-20 pb-6 w-full bg-purple-500 max-md:px-5 max-md:max-w-full"
+    >
+      <img
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/7ce1ecef26083e6e6b88ff9135dc78a53c85fd9565a37370ca5be43540fa388c?apiKey=1ae664018bfa43d7a4184d6c2cb38528&"
+        class="mt-24 ml-10 w-16 aspect-square fill-white max-md:mt-10 max-md:ml-2.5"
+      />
+    </div>
+    <div
+      class="flex justify-center items-center px-16 w-full bg-pink-400 max-md:px-5 max-md:max-w-full"
+    >
+      <div
+        class="flex z-10 gap-5 justify-between mt-0 mb-0 max-w-full w-[863px] max-md:flex-wrap max-md:mb-2.5"
+      >
+        <div
+          class="shrink-0 self-end mt-96 w-2 h-2 bg-white rounded-full max-md:mt-10"
+        ></div>
+        <div
+          class="flex justify-center items-center px-16 py-20 bg-red-100 rounded-xl max-md:px-5"
+        >
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fa8a24b50602aa03a4a43efc1e2580030dbde11b2a642a5620d184df99b0bd5?apiKey=1ae664018bfa43d7a4184d6c2cb38528&"
+            class="mt-4 w-20 aspect-[0.4]"
+          />
+        </div>
+        <div
+          class="flex justify-center items-center px-16 py-20 bg-blue-100 rounded-xl max-md:px-5"
+        >
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ba3b8d03cb8f35108169b5a56a15f5a156b3c990b95f3acc7185bb3aec486996?apiKey=1ae664018bfa43d7a4184d6c2cb38528&"
+            class="mt-4 w-20 aspect-[0.4]"
+          />
+        </div>
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ee87c13f6e9eaada64db19b6bcb4968c95f7fc8e067dbebda7f16444d4ce48b9?apiKey=1ae664018bfa43d7a4184d6c2cb38528&"
+          class="shrink-0 self-end mt-60 aspect-square fill-white w-[46px] max-md:mt-10 transform"
+        />
+      </div>
+    </div>
+    <div
+      class="flex flex-col items-center px-16 pt-20 w-full whitespace-nowrap bg-pink-300 max-md:px-5 max-md:max-w-full"
+    >
+      <div class="flex z-10 gap-3 mt-36 mb-0 max-md:mt-10 max-md:mb-2.5">
+        <div class="grow text-6xl bg-clip-text max-md:text-4xl">PLAY</div>
+        <div class="flex gap-1 my-auto text-5xl max-md:text-4xl">
+          <div class="bg-clip-text max-md:text-4xl">&lt;</div>
+          <div class="bg-clip-text max-md:text-4xl">&lt;</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
