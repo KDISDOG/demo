ca7e6cbb386b6a31f1a28a500bda788f18756c56
KDISDOG <sam90315@gmail.com>
2024-05-14 15:55:13 +0800

    0514

--- practice/0824calendar/calendar.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import Vue from 'vue';
-import VCalendar from 'v-calendar';
-
-Vue.use(VCalendar, {
-    componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
-                    // ...other defaults
-  });
---
+++ practice/piniaTest/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ stores.doubleCount }}</h1>
+  <button @click="stores.increment()">click</button>
+</template>
+
+<script setup>
+import { useCounterStore } from "./stores/counter";
+const stores = useCounterStore();
+console.log(stores.count);
+</script>
